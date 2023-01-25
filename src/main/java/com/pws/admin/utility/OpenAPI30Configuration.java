c58796bc21c3c156f15dab16cc3641bbe189afa8
the-devil666 <thedevilkr@gmail.com>
2023-01-25 17:39:32 +0530

    Admin-25/01/2023

--- src/main/java/com/pws/admin/utility/OpenAPI30Configuration.java
+++ src/main/java/com/pws/admin/utility/OpenAPI30Configuration.java
@@ -1,6 +1,8 @@
 package com.pws.admin.utility;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +14,10 @@ public class OpenAPI30Configuration {
     public OpenAPI customizeOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .info(new Info().title("Admin API")
+                        .description("Spring Admin sample application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
                 .components(new Components()
